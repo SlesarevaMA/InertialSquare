70b696cd9080e1182041302321a5942be1d8befb
Margarita Slesareva <slesareva.m.a@yandex.ru>
2024-02-16 12:59:43 +0300

    Project setup

--- InertialSquare/ViewController.swift
+++ InertialSquare/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private enum Metrics {
+    static let squareHeight: CGFloat = 100
+}
+
+final class ViewController: UIViewController {
+    
+    private let squareView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureViews()
     }
+    
+    private func configureViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(squareView)
+        
+        squareView.backgroundColor = .systemBlue
+        
+        let x = (view.frame.width - Metrics.squareHeight) / 2
+        let y = (view.frame.height - Metrics.squareHeight) / 2
 
-
+        squareView.frame = CGRect(x: x, y: y, width: Metrics.squareHeight, height: Metrics.squareHeight)
+        squareView.layer.cornerRadius = 10
+    }
 }
-
